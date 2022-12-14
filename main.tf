520954b1d32002649ad2b483eb6a1fc0d1cd9a02
Sai Wang <hotdeals2k6@hotmail.com>
2022-12-14 15:19:35 -0500

    internal ip address changed

--- main.tf
+++ main.tf
@@ -93,14 +93,14 @@ resource "aws_security_group" "allow_web" {
 # 7. Create a network interface with an ip in the subnet that was created in step 4
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.subnet-1.id
-  private_ips     = ["10.0.1.50"]
+  private_ips     = ["10.0.1.51"]
   security_groups = [aws_security_group.allow_web.id]
 }
 # 8. Assign an elastic IP to the network interface created in step 7
 resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
-  associate_with_private_ip = "10.0.1.50"
+  associate_with_private_ip = "10.0.1.51"
   depends_on                = [aws_internet_gateway.gw]
 }
 
